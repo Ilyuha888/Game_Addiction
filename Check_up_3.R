c51fc6de8eef26d1ed809bc836a5d0fcf4b4eb41
Nartov Ilya <nartov.nartov@gmail.com>
2023-05-12 14:56:58 +0300

    Предобработали

---
+++ Check_up_3.R
@@ -0,0 +1,146 @@
+library(tidyverse)
+
+
+#ЧИСТКА ДАННЫХ
+
+
+#Читаем таблицу
+gam <- read_csv2("gameadddiction.csv")
+#Создаём вектор с новыми именами
+new_names <- c('time','ID','sex','age', 'max_rating', 'current_rating', 'hours_in_game', 'games_week', 
+               'pas01', 'pas02', 'pas03', 'pas04', 'pas05', 'pas06', 'pas07', 'pas08', 'pas09', 
+               'rel01', 'rel02', 'rel03', 'rel04', 'rel05', 'rel06', 'rel07', 'rel08', 'rel09', 'rel10', 
+               'rel11', 'rel12', 'rel13', 'heroes_3')
+
+#Переименовываем все столбцы по-человечески
+colnames(gam) <- new_names
+#Проверяем, что вышло
+str(gam)
+View(gam)
+#Уберём точно ненужные колонки
+gam %>% select(-c(time, heroes_3)) -> gam 
+
+#Уберём строки с пропущенными значениями
+gam %>% drop_na() -> gam
+#Потеряли 1 строку
+#Проверили на всякий случай
+gam %>% is.na() %>% summary()
+
+#В столбцах age - hours_in_game должны быть только цифры, 
+#выкинем всё остальное (данных много, можем себе позволить)
+gam %>% filter((!is.na(as.numeric(age)) & 
+                    !is.na(as.numeric(max_rating)) &
+                    !is.na(as.numeric(current_rating)) & 
+                    !is.na(as.numeric(hours_in_game)))) -> gam
+str(gam)
+#Займёмся перегонко в хорошие форматы данных, которые есть
+#sex в фактор переведём
+gam %>% mutate(sex = as.factor(sex)) -> gam
+#age, max_rating, current_rating, hours_in_game в числа (потому что числа)
+gam %>% mutate(age = as.numeric(age),
+               max_rating = as.numeric(max_rating),
+               current_rating = as.numeric(current_rating),
+               hours_in_game = as.numeric(hours_in_game),) -> gam
+#Почистим шутников и ограничим возраст 6-100, 
+#рейтинг ограничим 11000 (самый большой в мире),
+#часов в игре 17000 (самое большое количество часов в мире),
+#уберём тех, у кого текущий рейтинг больше максимального
+
+gam %>% filter(age <101 &
+                 age > 5 &
+                 max_rating < 11000 &
+                 max_rating >= current_rating &
+                 hours_in_game < 17000) -> gam
+
+#games_week переведём в фактор
+gam %>% mutate(games_week = factor(games_week,
+                                  levels = c('1-5 игр (по выходным чисто пару каток)', '6-10 игр (пару дней в неделю стабильно-дота)', '11-20 игр (почти каждый день по несколько игр)', '20-30 игр (каждый день по несколько игр)', 'Больше 30 игр (по много пабликов каждый день)', 'Больше 50 игр (хочу быть как ILTW, тренируюсь каждый день нон-стоп)'), 
+                                  ordered = TRUE)) -> gam
+#Создадим столбец age_group
+gam %>% mutate(age_group = case_when(age < 18 ~ '17 лет или младше',
+                                     age < 21 ~ '18-20 лет',
+                                     age < 30 ~ '21-29 лет',
+                                     age < 40 ~ '30-39 лет',
+                                     age < 50 ~ '40-49 лет',
+                                     age < 60 ~ '50-59 лет',
+                                     age > 59 ~ '60 лет или старше')) -> gam
+#age_group переведём в фактор
+gam %>% mutate(age_group = factor(age_group,
+                                   levels = c('17 лет или младше', '18-20 лет', '21-29 лет', '30-39 лет', '40-49 лет', '50-59 лет', '60 лет или старше'), 
+                                   ordered = TRUE)) -> gam
+#Создадим столбец с итоговым баллом вовлечённости (зависимости)
+gam %>% mutate(pas_total = rowSums(across(c(pas01:pas09)))) -> gam
+
+#Сохраняем почищенный файл
+write_csv(gam, 'gam_clean.csv')
+
+
+#ОПИСАТЕЛЬНЫЕ СТАТИСТИКИ
+
+
+#Статистики по ЗП (уровень вовлечённости)
+gam %>% summarise(mean = mean(pas_total),
+                  median = median(pas_total),
+                  sd = sd(pas_total),
+                  min = min(pas_total),
+                  max = max(pas_total)) %>% 
+  write_csv('Статистики по ЗП (уровень вовлечённости).csv')
+#График
+gam %>% ggplot(aes(x=pas_total)) +
+  geom_histogram(aes(y=..density..)) + 
+  geom_density(aes(y=..density..)) +
+  geom_vline(xintercept=mean(gam$pas_total), size=1.0, color="red") +
+  geom_vline(xintercept=median(gam$pas_total), size=1.0, color="blue") +
+  geom_text(aes(x=mean(gam$pas_total)+7, , color="red", label=paste0("Среднее"), y=0.090)) +
+  geom_text(aes(x=mean(gam$pas_total)+7, , color="blue", label=paste0("Медиана"), y=0.080)) +
+  theme(legend.position = "none") + 
+  labs(x = "Уровень вовлечённости", y = "Плотность вероятности",
+       title = "Вовлечённость на выборке")
+
+#Статистики по НП (возрастные группы)
+#По фактору чёт не очень понятно, ограничимся граффиком
+gam %>% ggplot(aes(x=age_group)) +
+  geom_bar() + 
+  theme_bw() +
+  labs(x = "Возрастная группа", y = "Количество респондентов",
+       title = "Распределение по возрастам")
+
+#Статистики по НП (количество игр в неделю)
+#По фактору чёт не очень понятно, ограничимся граффиком
+gam %>% ggplot(aes(x=games_week)) +
+  geom_bar() + 
+  theme_bw() +
+  scale_x_discrete(labels=c("1-5", "6-10", "11-20", "20-30", "> 30", "> 50")) +
+  labs(x = "Количество игр в неделю", y = "Количество респондентов",
+       title = "Количество игр в неделю на выборке")
+
+#Статистики по НП (рейтинг - текущий)
+#Текущий рейтинг выбрали, потому что тестивароние проводится сейчас, 
+cor.test(gam$max_rating,gam$current_rating)
+#а корреляция очень значима и велика (0.93)
+gam %>% summarise(mean = mean(current_rating),
+                  median = median(current_rating),
+                  sd = sd(current_rating),
+                  min = min(current_rating),
+                  max = max(current_rating)) %>% 
+  write_csv('Статистики по НП (рейтинг - текущий).csv')
+#График
+gam %>% ggplot(aes(x=current_rating)) +
+  geom_histogram(aes(y=..density..)) + 
+  geom_density(aes(y=..density..)) +
+  geom_vline(xintercept=mean(gam$current_rating), size=1.0, color="red") +
+  geom_vline(xintercept=median(gam$current_rating), size=1.0, color="blue") +
+  geom_text(aes(x=mean(gam$current_rating)+1600, , color="red", label=paste0("Среднее"), y=0.00030)) +
+  geom_text(aes(x=mean(gam$current_rating)+1600, , color="blue", label=paste0("Медиана"), y=0.00027)) +
+  theme(legend.position = "none") + 
+  labs(x = "Текущий рейтинг", y = "Плотность вероятности",
+       title = "Текущий рейтинг на выборке")
+
+#Искомый график
+gam %>% ggplot(aes(x=current_rating, y = pas_total, color = games_week)) +
+  scale_color_manual(values = colorspace::rainbow_hcl(length(unique(diamonds$color))),
+                     labels = c("1-5", "6-10", "11-20", "20-30", "> 30", "> 50")) +
+  geom_point()+
+  geom_smooth(method = 'lm')+
+  facet_wrap( ~ age_group) + 
+  labs(x = "Текущий рейтинг", y = "Уровень вовлечённоси", title = "График искомых закономерностей") 
