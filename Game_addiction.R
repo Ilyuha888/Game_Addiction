ac79e0a74306440fda6a39c34fa7045f68ac7c99
Nartov Ilya <nartov.nartov@gmail.com>
2023-05-22 19:41:42 +0300

    Сделали анализ

--- Game_addiction.R
+++ Game_addiction.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-
+library(pwr)
 
 #ЧИСТКА ДАННЫХ
 
@@ -146,8 +146,8 @@ gam %>% ggplot(aes(x=current_rating)) +
   geom_density(aes(y=..density..)) +
   geom_vline(xintercept=mean(gam$current_rating), size=1.0, color="red") +
   geom_vline(xintercept=median(gam$current_rating), size=1.0, color="blue") +
-  geom_text(aes(x=mean(gam$current_rating)+1600, , color="red", label=paste0("Среднее"), y=0.00030)) +
-  geom_text(aes(x=mean(gam$current_rating)+1600, , color="blue", label=paste0("Медиана"), y=0.00027)) +
+  geom_text(aes(x=mean(gam$current_rating)+1600, , color="red", label="Среднее", y=0.00030)) +
+  geom_text(aes(x=mean(gam$current_rating)+1600, , color="blue", label="Медиана", y=0.00027)) +
   theme(legend.position = "none") + 
   labs(x = "Текущий рейтинг", y = "Плотность вероятности",
        title = "Текущий рейтинг на выборке")
@@ -162,3 +162,91 @@ gam %>% ggplot(aes(x=current_rating, y = pas_total, color = games_week)) +
   geom_smooth(method = 'lm')+
   facet_wrap( ~ age_group) + 
   labs(x = "Текущий рейтинг", y = "Уровень вовлечённоси", title = "График искомых закономерностей") 
+
+
+#ОБРАБОТКА ДАННЫХ
+#ЛИНЕЙНАЯ РЕГРЕССИЯ
+
+
+#График выше показал, что возрстные группые 40+ представлены очень слабо, поэтому мы их из анализа выкинем
+gam %>% filter(age_group %in% c('17 лет или младше', '18-20 лет', '21-29 лет', '30-39 лет')) -> gam
+#Начнём с малого. Посмотрим предикторы отдельно
+
+#Сначала рейтинг
+model1 <- lm(pas_total ~ current_rating, gam)
+summary(model1)
+plot(model1)
+#Модель слабовата, но стастически значима
+#Однако при очень большой выборке мы замечаем даже слабые эффекты, предсказывает 0.06% дисперсии
+
+#Теперь посмотрим возрастную группу
+model2 <- lm(pas_total ~ age_group, gam)
+summary(model2)
+plot(model2)
+#Здесь модель уже имеет большую значимость, но предсказывает очень маленькую дисперсию (0.8% дисперсии)
+#Значимы предиктором является принадлежность к первой и второй возрастным группам
+
+#Теперь посмотрим количество игр в неделю
+model3 <- lm(pas_total ~ games_week, gam)
+summary(model3)
+plot(model3)
+#Эта модель уже лучше, она предсказывает 15.8% дисперсии, кайф! (начало)
+#Значимые предикторы - это принадлженость к тем, кто играет 1-5, 6-10 и 11-20
+
+
+#Теперь посмотрим игры в неделю вместе с возрастной группой
+model4 <- lm(pas_total ~ games_week + age_group, gam)
+summary(model4)
+plot(model4)
+#Модель не лучше предыдущей, и значимы те-же предикторы, объясняет 16.0% дисперсии 
+#Проверим вздутость (если предикторы коррелируют между собой сильно)
+car::vif(model4)
+#Её нет
+
+#Теперь посмотрим все три
+model5 <- lm(pas_total ~ games_week + age_group + current_rating, gam)
+summary(model5)
+#Добавление рейтинга модель не улучшило(
+
+#Теперь посмотрим игры в неделю, возрастную группу и их взаимодействие
+model6 <- lm(pas_total ~ games_week + age_group + games_week*age_group, gam)
+summary(model6)
+#Модель оказалась хуже чем модель без взаимодействия... Объясняем 15.8% дисперсии
+#Проверим вздутость
+car::vif(model6)
+#Её нет, но задатки появляются
+plot(model6)
+
+
+#А теперь добавим в к ним рейтинг
+model7 <- lm(pas_total ~ games_week + age_group + current_rating + games_week*age_group, gam)
+summary(model7)
+#Модель плохая, предикторы лишние, гипотеза не подтверждается, мы плачем((((
+#Дисперсия 15.9%
+
+
+
+
+
+#Может быть был косяк с тем, что мы перевели шкалу отношений в ординаруную?
+#А давайте оставим возраст, как шкалу отношений и посмотрим
+model8 <- lm(pas_total ~ age, gam)
+summary(model8)
+#Модель объясняет 0.7% дисперсии. Ну тоже плохо (((
+
+#Теперь посмотрим игры в неделю, возраст и их взаимодействие
+model9 <- lm(pas_total ~ games_week + age + games_week*age, gam)
+summary(model9)
+#Модель объясняет 16% дисперсии и плохо интерпретируется
+
+#Нет, не был
+
+View(model1)
+#Посчитаем мощность для подтвердившихся гипотез
+#Возрастная группа
+pwr.f2.test(u = 3, v = 5717, sig.level = 0.01, f2 = 0.008/(1 - 0.008))
+#Игр в неделю
+pwr.f2.test(u = 5, v = 5715, sig.level = 0.01, f2 = 0.158/(1 - 0.158))
+
+
+
